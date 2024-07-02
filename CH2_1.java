256e502e1af78e966c734021a16086ba5d17d1d6
hank9371 <113032611+hank9371@users.noreply.github.com>
2024-07-02 11:51:51 +0800

    Add files via upload
    第一支java

---
+++ CH2_1.java
@@ -0,0 +1,12 @@
+public class CH2_1{
+    public static void main(String[] args){
+        int num1=2;
+        int num2=3;
+        float num3=4;
+        String num4="五";
+        System.out.println(num1+"cats are running");
+        System.out.println(num2+"dogs are running");
+        System.out.println(num3+"birds are running");
+        System.out.println(num4+"birds are running");
+    }
+}
